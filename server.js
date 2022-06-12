bea9a3bf1428a752a97d30bbb8540d5cdbd4c35a
idbic <dbicga@gmail.com>
2022-06-12 14:57:30 -0500

    index route is up and running

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 //require .env file
 // require("dotenv").config()
-// setting up express and liquid and all of the variables here
+
+//////////////////////////////////////////////
+//setting up express and liquid and all of the variables here
+//////////////////////////////////////////////// 
 const express = require('express')
 const app = require('liquid-express-views')(express())
 const PORT = 2022
+const dojoRouter = require('./controllers/dojostorm.js')
+
+
+
+
+//////////////////////////////////////////////
+// Index Route
+//////////////////////////////////////////////
+app.get('/index', (req, res) => {
+    res.render('index')
+})
 
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
 // const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Now Listening on port ${PORT} @dbic`));
+app.listen(PORT, () => {
+    console.log(`Now Listening on port ${PORT} @dbic`)
+});
 
